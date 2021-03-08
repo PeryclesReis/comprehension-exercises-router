f6b440555781c2ec974e8ff7386d28561b08e67a
Perycles floriano <perycles_reis@hotamil.com>
2021-03-08 20:29:52 -0300

    exercicios ate o 6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
+import About from './components/About';
+import Users from './components/Users';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/users'>Users</Link>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/about'>
+          <About />
+        </Route>
+        <Route path='/users'>
+          <Users />
+        </Route>
+      </BrowserRouter>
     );
   }
 }
